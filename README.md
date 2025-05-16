5ee4bfea864ab3134ccd21b2ee405bfca38c3cec
huma-v <13147304+huma-v@users.noreply.github.com>
2025-05-16 05:36:13 +0300

    Перша версія

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # Maps.epf
-Інтеграція 1С з сервісами геоданих
+
+Приклад інтеграції 1С з сервісами геоданих.
+
+Виконує наступні функції:
+
+* Відображення карти з інтерактивним вибором координат поточної шляхової точки
+* Геокодинг (адреса в координати)
+* Зворотній геокодинг (координати в адресу)
+* Планування маршруту з розрахунком орієнованого часу та відстані
+* Оптимізація маршруту
+
+# Підтримувані платформи
+
+Протестовано на 8.3 від 8.3.11, на Windows 7 (з встановленим Internet Explorer 11), 10, та 11
+**Поки підтримуються тільки звичайні форми**
+
+# Build
+
+1. Створити файл .env з наступним вмістом:
+    ```
+    1C_HOME=C:\Program Files[ (x86)]\1cv8\x.y.zz.wwww
+    ```
+2. Запустити файл build.ps1
+3. Файл обробки .epf має з'явитися в папці `build`
+
+# Використовувані технології
+
+Для відображення карт використовуєтья [leaflet.js](https://github.com/Leaflet/Leaflet), та наступні плагіни:
+* [Leaflet.markercluster](https://github.com/Leaflet/Leaflet.markercluster)
+* [Leaflet.GeometryUtil](https://github.com/makinacorpus/Leaflet.GeometryUtil)
+* [leaflet-arrowheads](https://github.com/slutske22/leaflet-arrowheads) ([Форк](https://github.com/huma-v/leaflet-arrowheads-es5))
+* [Leaflet Polyline Offset](https://github.com/bbecquet/Leaflet.PolylineOffset) ([Форк](https://github.com/higaa/Leaflet.PolylineOffset))
+
+Також використовуються сторонні API на яких можуть бути окремі обмеження використання:
+* [IPWHOIS.IO](https://ipwhois.io) - не підтримує безкоштовне використання в коммерційних цілях
+* [OpenStreetMap](www.openstreetmap.org)
+* [Nominatim](https://nominatim.org/)
+* [OSRM](https://project-osrm.org/)
+
+Якщо ви збираєтесь використовувати цю обробку в роботі, рекомендується закоментувати виклик ipwhois
+і перенаправити OSM, Nominatim, OSRM на локально розгорнуті сервіси
---
+++ build.ps1
@@ -0,0 +1,89 @@
+﻿$ErrorActionPreference = "Stop"
+
+# Read .env file into environment variables
+if (Test-Path .env -PathType Leaf) {
+    Get-Content .env | ForEach-Object {
+        $name, $value = $_.split('=')
+        Set-Item env:$name -Value $value
+    }
+}
+
+# Create build output directory
+New-Item .\build -ItemType Directory -Force
+
+New-Item .\build\download -ItemType Directory -Force
+
+# Download dependencies
+if (-not (Test-Path .\build\download\leaflet)) {
+	Invoke-WebRequest "https://github.com/Leaflet/Leaflet/releases/download/v1.8.0/leaflet.zip" -OutFile .\build\download\leaflet.zip
+	Expand-Archive .\build\download\leaflet.zip -DestinationPath .\build\download\leaflet\ -Force
+}
+
+
+if (-not (Test-Path .\build\download\Leaflet.markercluster-1.1.0)) {
+	Invoke-WebRequest "https://github.com/Leaflet/Leaflet.markercluster/archive/v1.1.0.zip" -Outfile .\build\download\Leaflet.markercluster.zip
+	Expand-Archive .\build\download\Leaflet.markercluster.zip -DestinationPath .\build\download\ -Force
+
+}
+
+if (-not (Test-Path .\build\download\Leaflet.GeometryUtil-0.10.3)) {
+    Invoke-WebRequest "https://github.com/makinacorpus/Leaflet.GeometryUtil/archive/0.10.3.zip" -Outfile .\build\download\Leaflet.GeometryUtil.zip
+    Expand-Archive .\build\download\Leaflet.GeometryUtil.zip -DestinationPath .\build\download\ -Force
+}
+
+if (-not (Test-Path .\build\download\leaflet-arrowheads-es5-bcc98c30a2196498fc36d27a6df418e3f5633bea)) {
+    Invoke-WebRequest "https://github.com/huma-v/leaflet-arrowheads-es5/archive/bcc98c30a2196498fc36d27a6df418e3f5633bea.zip" -Outfile .\build\download\leaflet-arrowheads.zip
+    Expand-Archive .\build\download\leaflet-arrowheads.zip -DestinationPath .\build\download\ -Force
+}
+
+# Using a fork that fixes the loop problem
+if (-not (Test-Path .\build\download\Leaflet.PolylineOffset-4a52e28b9c32ab9c850315d451ed470529ac55dd)) {
+    Invoke-WebRequest "https://github.com/higaa/Leaflet.PolylineOffset/archive/4a52e28b9c32ab9c850315d451ed470529ac55dd.zip" -Outfile .\build\download\Leaflet.PolylineOffset.zip
+    Expand-Archive .\build\download\Leaflet.PolylineOffset.zip -DestinationPath .\build\download\ -Force
+}
+
+Remove-Item .\build\resources -Recurse -Force -ErrorAction SilentlyContinue
+New-Item .\build\resources -ItemType Directory
+
+New-Item .\build\resources\leaflet -ItemType Directory
+@("leaflet.js", "leaflet.css", "images") |
+    ForEach-Object { ".\build\download\leaflet\${_}" } |
+    Copy-Item -Destination .\build\resources\leaflet\ -Recurse
+
+New-Item .\build\resources\Leaflet.markercluster -ItemType Directory
+@("MIT-LICENCE.txt", "dist\leaflet.markercluster.js", "dist\MarkerCluster.css", "dist\MarkerCluster.Default.css") |
+    ForEach-Object { ".\build\download\Leaflet.markercluster-1.1.0\${_}" } |
+    Copy-Item -Destination .\build\resources\Leaflet.markercluster\
+
+New-Item .\build\resources\Leaflet.GeometryUtil -ItemType Directory
+@("LICENSE", "src\leaflet.geometryutil.js") |
+    ForEach-Object { ".\build\download\Leaflet.GeometryUtil-0.10.3\${_}" } |
+    Copy-Item -Destination .\build\resources\Leaflet.GeometryUtil\
+
+New-Item .\build\resources\leaflet-arrowheads -ItemType Directory
+@("LICENSE", "src\leaflet-arrowheads.js") |
+    ForEach-Object { ".\build\download\leaflet-arrowheads-es5-bcc98c30a2196498fc36d27a6df418e3f5633bea\${_}" } |
+    Copy-Item -Destination .\build\resources\leaflet-arrowheads\
+
+New-Item .\build\resources\Leaflet.PolylineOffset -ItemType Directory
+@("LICENSE", "leaflet.polylineoffset.js") |
+    ForEach-Object { ".\build\download\Leaflet.PolylineOffset-4a52e28b9c32ab9c850315d451ed470529ac55dd\${_}" } |
+    Copy-Item -Destination .\build\resources\Leaflet.PolylineOffset\
+
+# Copy over extra assets
+Copy-Item .\assets .\build\resources\ -Recurse
+
+Compress-Archive .\build\resources\* .\build\resources.zip -Force
+
+Remove-Item .\build\MapsEpf -Recurse -ErrorAction SilentlyContinue
+Copy-Item .\MapsEpf .\build\MapsEpf -Recurse
+
+# Copy over the template file
+New-Item .\build\MapsEpf\MapsEpf\Templates\АрхивРесурсов\Ext -ItemType Directory
+Copy-Item .\build\resources.zip .\build\MapsEpf\MapsEpf\Templates\АрхивРесурсов\Ext\Template.bin
+
+# Build the epf
+$designer = Start-Process "${env:1C_HOME}\bin\1cv8.exe" "DESIGNER /LoadExternalDataProcessorOrReportFromFiles .\build\MapsEpf\MapsEpf.xml .\build\Maps.epf" -PassThru -Wait
+if ($designer.ExitCode -ne 0) {
+	throw "Error building epf!";
+}
